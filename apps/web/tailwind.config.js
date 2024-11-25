57baec1e9b6e2fed62d36a0ebb8937a1c4d8ad2d
hobeom <hobeom2049@gmail.com>
2024-11-25 16:13:54 +0900

    feat : 디자인 토큰 생성

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,124 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        blue100: "#F3F6FF",
+        blue300: "#B3C6FC",
+        blue400: "#7C9DF8",
+        blue500: "#698DF3",
+        blue600: "#698DF3",
+        blue700: "#5885FC",
+        blue800: "#5C7ADD",
+        gray50: "#F7F8FA",
+        gray100: "#EBEDF4",
+        gray200: "#DBDFE8",
+        gray300: "#C1C5CD",
+        gray400: "#A3A8B2",
+        gray500: "#8B9097",
+        gray600: "#787B85",
+        gray700: "#646670",
+        gray800: "#4A4D51",
+        gray900: "#212121",
+        red: "#EB535D",
+        blue: "#5599FF",
+        purple: "#B99AF790",
+      },
+      fontSize: {
+        h1: [
+          "20px",
+          {
+            lineHeight: "24px",
+            fontWeight: "600",
+          },
+        ],
+        h2: [
+          "17px",
+          {
+            lineHeight: "24px",
+            fontWeight: "600",
+          },
+        ],
+        h3: [
+          "16px",
+          {
+            lineHeight: "20px",
+            fontWeight: "600",
+          },
+        ],
+        h4: [
+          "15px",
+          {
+            lineHeight: "20px",
+            fontWeight: "600",
+          },
+        ],
+        b1: [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "500",
+          },
+        ],
+        b2: [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "500",
+          },
+        ],
+        b3: [
+          "14px",
+          {
+            lineHeight: "20px",
+            fontWeight: "400",
+          },
+        ],
+        b3_com: [
+          "14px",
+          {
+            lineHeight: "18px",
+            fontWeight: "400",
+          },
+        ],
+        b4: [
+          "13px",
+          {
+            lineHeight: "16px",
+            fontWeight: "500",
+          },
+        ],
+        b5: [
+          "13px",
+          {
+            lineHeight: "16px",
+            fontWeight: "400",
+          },
+        ],
+        c1: [
+          "12px",
+          {
+            lineHeight: "14px",
+            fontWeight: "500",
+          },
+        ],
+        c2: [
+          "12px",
+          {
+            lineHeight: "16px",
+            fontWeight: "500",
+          },
+        ],
+        c3: [
+          "11px",
+          {
+            lineHeight: "14px",
+            fontWeight: "500",
+          },
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
