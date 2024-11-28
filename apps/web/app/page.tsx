96901a5967839576a70d59e357cb5c47a1e9b499
hobeom <hobeom2049@gmail.com>
2024-11-28 12:39:29 +0900

    fix : 캘린더 패키지 오류 수정

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function page() {
   return (
-    <Button>
+    <Button className="bg-warning">
       <Link href="/design">Go to Design</Link>
     </Button>
   );
