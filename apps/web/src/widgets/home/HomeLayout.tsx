5dab1bd907a6601342236bacd7198dd591f88499
GuNyangE <noel7145@gmail.com>
2024-11-28 14:11:34 +0900

    각종 설정 다시 적용

--- apps/web/src/widgets/home/HomeLayout.tsx
+++ apps/web/src/widgets/home/HomeLayout.tsx
@@ -1,7 +1,18 @@
+import localFont from 'next/font/local';
+
+const pretendard = localFont({
+  src: '../../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
 export function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex flex-col px-4 m-auto h-screen w-mobile">{children}</body>
+      <body className={`flex flex-col px-4 m-auto h-screen w-mobile ${pretendard.variable} font-pretendard`}>
+        {children}
+      </body>
     </html>
   );
 }
