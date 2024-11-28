ec98ebe0e4987783442c41828e17ed45579f7720
muse9312 <muse9312@naver.com>
2024-11-28 20:56:40 +0900

    feat : ItemCard component modify

--- apps/web/app/itemCard/page.tsx
+++ apps/web/app/itemCard/page.tsx
@@ -12,6 +12,15 @@ export default function page({}: Props) {
       title: '골든볼 팝업스토어',
       day: '05.21(금) - 12.31(일)',
       deadLine: 'D-40',
+      isCount: true,
+    },
+    {
+      id: 2,
+      img: '/images/img-card-2.png',
+      location: '서울 성동구',
+      title: '어노브 이터널 아우라 성수 팝업스토어',
+      day: '11.08(금) - 11.24(일)',
+      deadLine: 'D-3',
       isCount: false,
     },
   ];
@@ -19,7 +28,7 @@ export default function page({}: Props) {
   return (
     <>
       {data.map(data => (
-        <div w-full key={data.id}>
+        <div className="mb-8" key={data.id}>
           <ItemCard
             img={data.img}
             location={data.location}
--- apps/web/src/shared/ui/ItemCard.tsx
+++ apps/web/src/shared/ui/ItemCard.tsx
@@ -9,32 +9,34 @@ type Props = {
   isCount: boolean;
 };
 
+// type VariantType = 'text' | 'primary' |'secondary' | 'outlined' | 'fillOutlined' |'notice' | 'gradient' | 'help' | 'box' | 'personal'
+// type size = 'sm' | 'md' | 'lg' ('primary' |'secondary' | 'outlined' 용도)
+// type severityType = 'error' | 'warning' | info | 'success
+
 export const ItemCard = ({ img, location, title, day, deadLine, isCount }: Props) => {
   return (
     <div className="flex flex-col w-full gap-2 ">
       {/* 이미지 섹션 */}
-      <div className=" w-[160px] h-[160px]">
-        <Image
-          src={img}
-          width={'160'}
-          height={160}
-          alt={title}
-          objectFit="cover" // 이미지 비율 유지
-          className="rounded-t-lg" // 이미지 상단 모서리 둥글게 처리
-        />
+      <div className="relative">
+        <Image src={img} width={160} height={160} alt={title} objectFit="cover" className="rounded-sm" />
+        {isCount && (
+          <div className="absolute bottom-0 right-0  px-[8.5px] py-[5px] bg-purple-500 text-c3 text-white">
+            마감임박
+          </div>
+        )}
       </div>
 
       {/* 텍스트 섹션 */}
       <div className="flex flex-col gap-[2px]">
-        <span className="text-sm text-gray-600">{location}</span>
-        <span className="text-base font-bold text-gray-900">{title}</span>
-        <span className="text-sm text-gray-500">{day}</span>
+        <span className="text-gray-600 text-b4">{location}</span>
+        <span className="overflow-hidden text-gray-900 text-h4 whitespace-nowrap text-ellipsis">{title}</span>
+        <span className="text-gray-500 text-b5">{day}</span>
       </div>
 
       {/* 버튼 섹션 */}
-      <button type="button" className="w-[64px] h-[24px] bg-gray-300 text-c2 text-blue-600 text-white rounded ">
+      <div className="flex justify-center items-center w-[64px] h-[24px] text-blue-600 text-c2 bg-blue-100  rounded-sm ">
         종료 {deadLine}
-      </button>
+      </div>
     </div>
   );
 };
