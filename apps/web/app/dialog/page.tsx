5cfec812cf99a0b69657d845b05e91d6a94f7e5e
GuNyangE <noel7145@gmail.com>
2024-12-06 19:11:15 +0900

    POP-29 dialog 구현중

---
+++ apps/web/app/dialog/page.tsx
@@ -0,0 +1,42 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  CheckboxButton,
+  LikeIconButton,
+  PrimaryButton,
+} from '@/src/shared';
+import React from 'react';
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <AlertDialog>
+        <AlertDialogTrigger>예약 취소하기</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our
+              servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default page;
