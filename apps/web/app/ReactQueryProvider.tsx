737e64d0c16b580510f6e22803516a47073bbca4
muse9312 <muse9312@naver.com>
2024-12-18 16:50:37 +0900

    feat : react-query & .env apply

---
+++ apps/web/app/ReactQueryProvider.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+type Props = {};
+
+export const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head';
+'use client';
+
 import '../styles/globals.css';
 import localFont from 'next/font/local';
+import { ReactQueryProvider } from './ReactQueryProvider';
 
 const pretendard = localFont({
   src: '../public/fonts/PretendardVariable.woff2',
@@ -11,8 +13,12 @@ const pretendard = localFont({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`min-w-[320px] h-screen max-w-[780px] ${pretendard.variable} font-pretendard`}>{children}</body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={`min-w-[320px] h-screen max-w-[780px] ${pretendard.variable} font-pretendard`}>
+          {children}
+        </body>
+      </html>
+    </ReactQueryProvider>
   );
 }
