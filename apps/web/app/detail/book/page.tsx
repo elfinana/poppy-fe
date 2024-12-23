6ccc203cb2952f67924b7c9b67277fefefa789c6
hobeom <hobeom2049@gmail.com>
2024-12-23 14:38:48 +0900

    fix : 문법 맞춤

--- apps/web/app/detail/book/page.tsx
+++ apps/web/app/detail/book/page.tsx
@@ -1,5 +1,5 @@
+import CheckoutPage from '@/src/views/book/CheckoutPage';
 import React from 'react';
-import CheckoutPage from '../../../src/views/book/CheckoutPage';
 
 const page = () => {
   return <CheckoutPage />;
