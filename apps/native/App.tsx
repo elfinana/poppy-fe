9a5312ddfa56a9e1229fff62015ea2f5f4b47178
GuNyangE <noel7145@gmail.com>
2025-01-23 18:44:56 +0900

    POP-54 모바일(안드로이드) 알림 설정

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -2,12 +2,18 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Button } from '@repo/ui';
 import { WebView } from 'react-native-webview';
+import { usePushNoticiations } from './usePushNotifications';
 
 export default function Native() {
+  const { expoPushToken, notification } = usePushNoticiations();
+
+  const data = JSON.stringify(notification, undefined, 2);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView style={styles.container} source={{ uri: 'https://poppy-awdiui9az-poppy-ca4d5978.vercel.app/home' }} />
-      {/* <Text>hello world</Text> */}
+      {/* <WebView style={styles.container} source={{ uri: 'https://poppy-b1v4jfcia-poppy-ca4d5978.vercel.app/home' }} /> */}
+      <Text>Token: {expoPushToken?.data}</Text>
+      <Text>{data}</Text>
     </SafeAreaView>
   );
 }
